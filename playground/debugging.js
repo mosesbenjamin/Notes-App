0d219a637f969eb30ad060937fcebbe42c67f23c
mosesbenjamin <mavewreek@gmail.com>
2020-09-19 00:39:12 +0100

    Debugging via CLI & Chrome

--- playground/debugging.js
+++ playground/debugging.js
@@ -9,3 +9,9 @@ debugger;
 person.name = 'Dave';
 
 console.log(person)
+
+
+// Debug commands
+//Via CLI- Run : node/nodemon inspect [name of app].js 
+//Via Chrome- Run: node/nodemon --inspect-brk [name of app].js 
+//And place the "debugger" key word where you wish to debug(use c, to continue)
