485ca02f9cb458b345ec56fac887f92541aea739
mosesbenjamin <mavewreek@gmail.com>
2020-09-18 21:54:36 +0100

    Simplifying inputs with yargs

--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.add = (a, b)=>{
-    console.log('addNote');
-    return "New note"
+var addNote = (title, body)=>{
+    console.log('Adding note', title, body);
+}
+
+var getAll = ()=>{
+    console.log('Geting all notes');
+}
+
+var getNote = (title)=>{
+    console.log('Getting note', title)
+}
+
+var removeNote = (title)=>{
+    console.log('Removing note', title)
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 }
